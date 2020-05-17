af30649b47e873b5cc9c8302f33a9c5c44301d90
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-16 18:29:55 -0700

    add syntax to divide two numbers

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+function divide(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 / input2;
+}
+
 function multiply(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
