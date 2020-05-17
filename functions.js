c14f4ec89fd365fe18d81f78b9081b7bb23d0c06
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-16 18:37:36 -0700

    add syntax to increment a number by one

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+function increment(input1) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + 1;
+}
+
 function divide(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
