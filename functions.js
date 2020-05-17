5c6537e56d8dfda36c4ff6398f29515f710c2344
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-16 20:28:11 -0700

    add concatenate with plus operator

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+function concatenatep(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return input1 + input2;
+}
+
 function remainder(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
