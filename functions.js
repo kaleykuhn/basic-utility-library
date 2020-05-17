3b7b36aeedf2be3e761e293c7773c495baa1a6ee
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-17 00:18:28 -0700

    add concatenate += syntax

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+function concatenat(input1, input2) {
+   // A1: any JavaScript value
+   // A2: any JavaScript value
+   // R: a single JavaScript value
+   return (input1 += input2);
+}
+
 function concatenatep(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
