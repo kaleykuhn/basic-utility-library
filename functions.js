7a184d9fe623ece074785477316ffb3443cddb6e
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-18 21:52:53 -0700

    add stand in line and edit unshift function

--- functions.js
+++ functions.js
@@ -1,10 +1,19 @@
-function unshift(input1, input2) {
+function standInLine(input1, input2) {
    // A1: any JavaScript value
    // A2: any JavaScript value
    // R: a single JavaScript value
+   input1.push(input2);
+   var removed = input1.shift();
+   return removed;
+}
 
-   input1.unshift(input2);
-   return input1;
+function unshift(arr, elem) {
+   // A1: array
+   // A2: element
+   // R: a single JavaScript value
+
+   arr.unshift(elem);
+   return arr;
 }
 
 function shift(input1) {
@@ -31,18 +40,28 @@ function push(input1, input2) {
    return input1;
 }
 
-function findNthToLast(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1[input1.length - input2];
+function findNthToLast(str, num) {
+   // A1: string
+   // A2: number
+   // R: a character of a string
+
+   var lengthOfString = str.length;
+   var nthToLastNum = lengthOfString - num;
+   var nthLetter = str[nthToLastNum];
+   return nthLetter;
 }
 
-function lastCharacter(input1) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1[input1.length - 1];
+function lastCharacter(str) {
+   // A1: string
+   // R: a character of a string
+   var lengthOfString = str.length;
+   var lastNum = lengthOfString - 1;
+   var lastLet = str[lastNum];
+   console.log(lastLet);
+
+   //var lastLet = str[str.length - 1];
+   return lastLet;
+   //return str[str.length - 1];
 }
 
 function nth(input1, input2) {
@@ -66,79 +85,80 @@ function length(input1, input2) {
    return (input1 = input2.length);
 }
 
-function construct(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-
-   return input1 + input2;
+function construct(str, var1) {
+   // A1: variable
+   // A2: string
+   // R: string
+   var ourVar = var1;
+   var ourStr = str;
+   return str + var1;
 }
 
-function concatenat(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return (input1 += input2);
+function concatenat(str1, str2) {
+   // A1: string
+   // A2: string
+   // R:   a new string
+   return (str1 += str2);
 }
 
-function concatenatep(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 + input2;
+function concatenatep(str1, str2) {
+   // A1: string
+   // A2: string
+   // R: a new string
+   return str1 + str2;
 }
 
-function remainder(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 / input2;
+function remainder(dec1, dec2) {
+   // A1: number
+   // A2: number
+   // R: remainder number
+   return dec1 / dec2;
 }
 
-function product(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 * input2;
+function product(dec1, dec2) {
+   // A1: decimal number
+   // A2: decimal number
+   // R: decimal number
+   return dec1 * dec2;
 }
 
-function decrement(input1) {
-   // A1: any JavaScript value
-   // R: a single JavaScript value
-   input1--;
-   return input1;
+function decrement(num) {
+   // A1: number
+   // R: number
+   num--;
+   return num;
 }
-function increment(input1) {
-   // A1: any JavaScript value
-   // R: a single JavaScript value
-   input1++;
-   return input1;
-}
-
-function divide(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 / input2;
+function increment(num) {
+   // A1: number
+   // R: number
+   num++;
+   return num;
 }
 
-function multiply(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 * input2;
+function quotient(num1, num2) {
+   // A1: number
+   // A2: number
+   // R: number
+   return num1 / num2;
 }
 
-function subtract(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 - input2;
+function multiply(num1, num2) {
+   // A1: number
+   // A2: number
+   // R: number
+   return num1 * num2;
 }
 
-function add(input1, input2) {
-   // A1: any JavaScript value
-   // A2: any JavaScript value
-   // R: a single JavaScript value
-   return input1 + input2;
+function subtract(num1, num2) {
+   // A1: number
+   // A2: number
+   // R: number
+   return num1 - num2;
+}
+
+function add(num1, num2) {
+   // A1: number
+   // A2: number
+   // R: number
+   return num1 + num2;
 }
